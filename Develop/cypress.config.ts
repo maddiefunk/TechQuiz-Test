f7fc4fa6a96fa570b9b9bb7f7fcf41d42b3d0156
Rose <madeline.funk91@gmail.com>
2025-06-03 18:42:36 -0500

    Deleted/added files

--- Develop/cypress.config.ts
+++ Develop/cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress';
+import customViteConfig from './vite.config'
 
 export default defineConfig({
   e2e: {
@@ -6,12 +7,12 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts,jsx,tsx}',
   },
   component: {
     devServer: {
       framework: 'react',
       bundler: 'vite', // or 'webpack', depending on your project
+      viteConfig: customViteConfig
     },
     specPattern: 'cypress/component/**/*.cy.{js,ts,jsx,tsx}',
   },
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TechQuiz-Test
-take the starter codebase of a fully functioning Tech Quiz application and enhance it by adding Cypress for both component and end-to-end tests
+
+## Description 
+This application takes the starter codebase of a fully functioning Tech Quiz application and enhances it by adding Cypress for both component and end-to-end tests. 
+
+## Demonstration Video 
+
+## Questions
+Please feel free to contact me at:
+Email - madeline.funk91@gmail.com
+Github - https://github.com/maddiefunk
